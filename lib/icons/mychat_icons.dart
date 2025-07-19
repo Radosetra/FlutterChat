c0caeddaa611a43a7dbc61c96ba34c1c1ccfaffe
Radosetra <mickaelradosetraraveloson@gmail.com>
2025-07-19 22:42:01 +0300

    feature: add icons

--- lib/icons/mychat_icons.dart
+++ lib/icons/mychat_icons.dart
@@ -1,4 +1,5 @@
 class MyChatIcons {
+  static const String arrowLeft = 'assets/icons/arrow-left.svg';
   static const String arrowUpRight = 'assets/icons/arrow-up-right.svg';
   static const String arrowUp = 'assets/icons/arrow-up.svg';
   static const String barChart = 'assets/icons/barChart2.svg';
@@ -7,10 +8,12 @@ class MyChatIcons {
   static const String chevronDown = 'assets/icons/chevron-down.svg';
   static const String chevronUp = 'assets/icons/chevron-up.svg';
   static const String messageCircle = 'assets/icons/message-circle.svg';
+  static const String mic = 'assets/icons/mic.svg';
   static const String moreVertical = 'assets/icons/more-vertical.svg';
   static const String navigation = 'assets/icons/navigation.svg';
   static const String plus = 'assets/icons/plus.svg';
   static const String search = 'assets/icons/search.svg';
+  static const String send = 'assets/icons/send.svg';
   static const String setting = 'assets/icons/settings.svg';
   static const String user = 'assets/icons/user.svg';
 }
