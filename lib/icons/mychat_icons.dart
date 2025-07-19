b3758abb9327bf1afe9525a4373642973f4f1be4
Radosetra <mickaelradosetraraveloson@gmail.com>
2025-07-19 12:12:54 +0300

    config: setup icons

---
+++ lib/icons/mychat_icons.dart
@@ -0,0 +1,16 @@
+class MyChatIcons {
+  static const String arrowUpRight = 'assets/icons/arrow-up-right.svg';
+  static const String arrowUp = 'assets/icons/arrow-up.svg';
+  static const String barChart = 'assets/icons/barChart2.svg';
+  static const String bell = 'assets/icons/bell.svg';
+  static const String check = 'assets/icons/check.svg';
+  static const String chevronDown = 'assets/icons/chevron-down.svg';
+  static const String chevronUp = 'assets/icons/chevron-up.svg';
+  static const String messageCircle = 'assets/icons/message-circle.svg';
+  static const String moreVertical = 'assets/icons/more-vertical.svg';
+  static const String navigation = 'assets/icons/navigation.svg';
+  static const String plus = 'assets/icons/plus.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String setting = 'assets/icons/settings.svg';
+  static const String user = 'assets/icons/user.svg';
+}
