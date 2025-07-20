8f8fee7fa0f744f86700724fb483af123c29ed46
Radosetra <mickaelradosetraraveloson@gmail.com>
2025-07-20 23:56:45 +0300

    config: Riverpod and Socket

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_chat/config/app_router.dart';
+import 'package:my_chat/presentation/common/providers/socket_service_provider.dart';
 
 void main() {
-  runApp(MainApp());
+  runApp(const ProviderScope(
+    child: MainApp(),
+  ));
 }
 
-class MainApp extends StatelessWidget {
-  MainApp({super.key});
+class MainApp extends ConsumerStatefulWidget  {
+  const MainApp({super.key});
 
+  @override
+  ConsumerState<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends ConsumerState<MainApp> {
   final _appRouter = AppRouter();
+
+  @override
+  void initState() {
+    super.initState();
+    final socketService = ref.read(socketServiceProvider);
+    socketService.connect();
+  }
 
   @override
   Widget build(BuildContext context) {
