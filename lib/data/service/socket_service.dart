d0764623e81acac603c19b6f314faaa93cddb022
Radosetra <mickaelradosetraraveloson@gmail.com>
2025-07-25 00:42:38 +0300

    feature: handle actif user and room discussion

--- lib/data/service/socket_service.dart
+++ lib/data/service/socket_service.dart
@@ -1,6 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_chat/presentation/chat/state/message_provider.dart';
+import 'package:my_chat/presentation/common/providers/user_state_provider.dart';
 import 'package:my_chat/types/message.dart';
+import 'package:my_chat/types/user.dart';
+import 'package:my_chat/utils/generate_roomid.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketService {
@@ -21,6 +24,24 @@ class SocketService {
       print('Connected to backend!');
     });
 
+    socket.on('activeUsers', (data) {
+      final users = (data as List).map((u) => User.fromJson(u)).toList();
+      ref.read(userProvider.notifier).setActiveUsers(users);
+    });
+  }
+
+  void joinActifUsers(User user){
+    socket.emit('join', user.toJson());
+  }
+
+  void sendMessage(Message message) {
+    socket.emit('sendMessage', message.toJson());
+  }
+
+  void joinRoom(String userA, String userB) {
+    final roomId = generateRoomId(userA, userB);
+    socket.emit('joinRoom', roomId);
+    socket.off('receiveMessage');
     socket.on('receiveMessage', (data) {
       print('New message: $data');
 
@@ -29,14 +50,10 @@ class SocketService {
           senderId: data['senderId'],
           senderName: data['senderName'],
           content: data['content'],
+          roomId: data['roomId'],
           timestamp: DateTime.now(),
         ),
       );
     });
-  }
-
-  void sendMessage(Message message) {
-    
-    socket.emit('sendMessage', message.toJson());
   }
 }
