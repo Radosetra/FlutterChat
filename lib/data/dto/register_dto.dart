c777be6cfaed816db5a691c2a50d8d7ad7e27adc
Radosetra <mickaelradosetraraveloson@gmail.com>
2025-09-22 21:44:14 +0300

    feature(auth dto): add dto

---
+++ lib/data/dto/register_dto.dart
@@ -0,0 +1,19 @@
+class RegisterDto {
+  final String username;
+  final String email;
+  final String password;
+
+  RegisterDto({
+    required this.username,
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'username': username,
+      'email': email,
+      'password': password,
+    };
+  }
+}
